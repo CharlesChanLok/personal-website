0750c0cb890d4648c9ee898d72f41a907e173bce
CharlesChanLok <charleschanlok@gmail.com>
2018-06-11 20:38:57 +0800

    clean up

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,62 +1,134 @@
-import React from 'react';
-import { Card, CardBody, CardTitle, Col, Container, Row } from 'reactstrap';
-import './Skill.css';
+import React from "react";
+import { Card, CardBody, CardTitle, Col, Container, Row } from "reactstrap";
+import "./Skill.css";
 
-const Skill = (props) => {
+const Skill = props => {
   return (
-    <div className='Skill'>
+    <div className="Skill">
       <Card>
         <CardBody>
           <CardTitle>Technologies Charles used</CardTitle>
           <Container>
             <Row>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png" alt="html5" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png"
+                  alt="html5"
+                />
                 <p>HTML5</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/2000px-CSS.3.svg.png" alt="css3" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/2000px-CSS.3.svg.png"
+                  alt="css3"
+                />
                 <p>CSS3</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Javascript_badge.svg/2000px-Javascript_badge.svg.png" alt="javascript" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Javascript_badge.svg/2000px-Javascript_badge.svg.png"
+                  alt="javascript"
+                />
                 <p>Javascript</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Font_C.svg/2000px-Font_C.svg.png" alt="c" />
-                <p>C</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2000px-Node.js_logo.svg.png" alt="nodejs" />
-                <p>Nodejs</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/640px-Npm-logo.svg.png" alt="npm" />
-                <p>NPM</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="react" />
-                <p>React</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/2000px-Git-logo.svg.png" alt="git" />
-                <p>Git</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://loiane.gallerycdn.vsassets.io/extensions/loiane/ts-extension-pack/0.0.1/1503435730489/Microsoft.VisualStudio.Services.Icons.Default" alt="typescript" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://loiane.gallerycdn.vsassets.io/extensions/loiane/ts-extension-pack/0.0.1/1503435730489/Microsoft.VisualStudio.Services.Icons.Default"
+                  alt="typescript"
+                />
                 <p>Typescript</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="http://clipartist.net/social/clipartist.net/P/postgresql_logo-555px.png" alt="postgresql" />
-                <p>Postgresql</p>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Font_C.svg/2000px-Font_C.svg.png"
+                  alt="c"
+                />
+                <p>C</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://www.elastic.io/wp-content/uploads/2018/02/REST-API-connector.png" alt="restapi" />
-                <p> REST API</p>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2000px-Node.js_logo.svg.png"
+                  alt="nodejs"
+                />
+                <p>Nodejs</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_1489714432/postman.png" alt="postman" />
-                <p>Postamn</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://camo.githubusercontent.com/30c7758f9ac63488d3c2814ed2dcd9dbb7e967f3/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f6865726f6b752d312e737667" alt="heroku" />
-                <p>Heroku</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://stackjava.com/wp-content/uploads/2018/03/bcrypt-logo.jpg" alt="bscrypt" />
-                <p>Bcrypt</p>
-              </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://rishabh.io/recipes/img/004-express-js.png" alt="express" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://rishabh.io/recipes/img/004-express-js.png"
+                  alt="express"
+                />
                 <p>Expressjs</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/AJAX_logo_by_gengns.svg/1200px-AJAX_logo_by_gengns.svg.png" alt="ajax" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+                  alt="react"
+                />
+                <p>React</p>
+              </Col>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/640px-Npm-logo.svg.png"
+                  alt="npm"
+                />
+                <p>NPM</p>
+              </Col>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/2000px-Git-logo.svg.png"
+                  alt="git"
+                />
+                <p>Git</p>
+              </Col>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="http://clipartist.net/social/clipartist.net/P/postgresql_logo-555px.png"
+                  alt="postgresql"
+                />
+                <p>Postgresql</p>
+              </Col>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://www.elastic.io/wp-content/uploads/2018/02/REST-API-connector.png"
+                  alt="restapi"
+                />
+                <p> REST API</p>
+              </Col>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/AJAX_logo_by_gengns.svg/1200px-AJAX_logo_by_gengns.svg.png"
+                  alt="ajax"
+                />
                 <p>AJAX</p>
+              </Col>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_1489714432/postman.png"
+                  alt="postman"
+                />
+                <p>Postamn</p>
+              </Col>
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://stackjava.com/wp-content/uploads/2018/03/bcrypt-logo.jpg"
+                  alt="bscrypt"
+                />
+                <p>Bcrypt</p>
               </Col>
             </Row>
           </Container>
@@ -65,25 +137,57 @@ const Skill = (props) => {
 
       <Card>
         <CardBody>
-          <CardTitle>Technologies or knowledge Charles wants to learn more</CardTitle>
+          <CardTitle>
+            Technologies or knowledge Charles wants to learn more
+          </CardTitle>
           <Container>
             <Row>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://equimper.gallerycdn.vsassets.io/extensions/equimper/react-native-react-redux/1.9.0/1526851136093/Microsoft.VisualStudio.Services.Icons.Default" alt="react redux" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://equimper.gallerycdn.vsassets.io/extensions/equimper/react-native-react-redux/1.9.0/1526851136093/Microsoft.VisualStudio.Services.Icons.Default"
+                  alt="react redux"
+                />
                 <p>Redux (State management)</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://loiane.gallerycdn.vsassets.io/extensions/loiane/ts-extension-pack/0.0.1/1503435730489/Microsoft.VisualStudio.Services.Icons.Default" alt="typescript" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://loiane.gallerycdn.vsassets.io/extensions/loiane/ts-extension-pack/0.0.1/1503435730489/Microsoft.VisualStudio.Services.Icons.Default"
+                  alt="typescript"
+                />
                 <p>Typescript</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://cdn4.iconfinder.com/data/icons/redis-2/1451/Untitled-2-512.png" alt="redis" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://cdn4.iconfinder.com/data/icons/redis-2/1451/Untitled-2-512.png"
+                  alt="redis"
+                />
                 <p>Redis (Caching)</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://cdn.iconscout.com/public/images/icon/premium/png-512/cyber-security-encryption-network-protection-smart-technology-3bd14d47ad627661-512x512.png" alt="Cyber security" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://cdn.iconscout.com/public/images/icon/premium/png-512/cyber-security-encryption-network-protection-smart-technology-3bd14d47ad627661-512x512.png"
+                  alt="Cyber security"
+                />
                 <p>Cyber security</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/2000px-Amazon_Web_Services_Logo.svg.png" alt="AWS" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/2000px-Amazon_Web_Services_Logo.svg.png"
+                  alt="AWS"
+                />
                 <p>Amazon Web Services</p>
               </Col>
-              <Col className='Skill-box'><img className='Skill-icon-box' src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/2000px-Tux.svg.png" alt="Linux" />
+              <Col className="Skill-box">
+                <img
+                  className="Skill-icon-box"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/2000px-Tux.svg.png"
+                  alt="Linux"
+                />
                 <p>Linux (System Administration)</p>
               </Col>
             </Row>
@@ -91,8 +195,6 @@ const Skill = (props) => {
         </CardBody>
       </Card>
     </div>
-
-
   );
 };
 
