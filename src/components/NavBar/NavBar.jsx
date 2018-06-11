23b01dd94bcd93828336afd9cfa3f24836d6b375
CharlesChanLok <charleschanlok@gmail.com>
2018-06-11 20:22:47 +0800

    commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
-import './NavBar.css';
-import About from '../About/About';
-import Contact from '../Contact/Contact';
-import Portfolio from '../Portfolio/Portfolio';
-import Skill from '../Skill/Skill';
-import Hobby from '../Hobby/Hobby';
-import ErrorPage from '../ErrorPage/ErrorPage';
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
+import "./NavBar.css";
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+import Portfolio from "../Portfolio/Portfolio";
+import Skill from "../Skill/Skill";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 export default class NavBar extends React.Component {
   constructor(props) {
@@ -33,22 +32,25 @@ export default class NavBar extends React.Component {
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
-                <Link className="NavBar-padding" to="/">About</Link>
-                <Link className="NavBar-padding" to="/Portfolio">Portfolio</Link>
-                <Link className="NavBar-padding" to="/Skill">Skill</Link>
-                <Link className="NavBar-padding" to="/Hobby">Hobby</Link>
-                <Link className="NavBar-padding" to="/Contact">Contact</Link>
+                <Link className="NavBar-padding" to="/">
+                  About
+                </Link>
+                <Link className="NavBar-padding" to="/Portfolio">
+                  Portfolio
+                </Link>
+                <Link className="NavBar-padding" to="/Skill">
+                  Skill
+                </Link>
+                <Link className="NavBar-padding" to="/Contact">
+                  Contact
+                </Link>
               </Nav>
             </Collapse>
           </Navbar>
-          <Switch>
-            <Route exact path="/" component={About} />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route path="/skill" component={Skill} />
-            <Route path="/hobby" component={Hobby} />
-            <Route path="/contact" component={Contact} />
-            <Route component={ErrorPage} />
-          </Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/skill" component={Skill} />
+          <Route path="/contact" component={Contact} />
         </div>
       </Router>
     );
